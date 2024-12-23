96ecbc9ad64ab6dcdcbee794a6fb926d3c22e846
David Marcos <ddragondavi@gmail.com>
2024-12-23 16:07:40 -0300

    feat: config tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,19 @@
 import type { Config } from "tailwindcss";
 
-export default {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+const config: Config = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-  
+      colors: {
+        text: "var(--color-text)",
+        error: "var(--color-error)",
+        paper: "var(--color-paper)",
+        primary: "var(--color-primary)",
+        background: "var(--color-background)",
+        "primary-contrast": "var(--color-primary-contrast)",
+      }
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
